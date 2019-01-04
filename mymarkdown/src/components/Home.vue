7bf0a33000abf6d5a73519ceefb32a16aa899a54
tajima <tajima_taichi@cyberagent.co.jp>
2019-01-04 23:56:31 +0900

    add vue components

---
+++ mymarkdown/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="home">
+    <h1>{{ msg }}</h1>
+    <button>Google アカウントでログイン</button>
+  </div>
+ </template>
+ <script>  
+ export default {   
+     name: 'home',
+     data () {
+        return {
+        msg: 'Welcome to MyMarkdown'
+        }
+     }
+  }
+  </script>
