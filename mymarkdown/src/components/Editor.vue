566112d94be12875fde213f8eb029eeb63088312
tajima <tajima_taichi@cyberagent.co.jp>
2019-01-07 03:40:22 +0900

    modify Editor.vue

--- mymarkdown/src/components/Editor.vue
+++ mymarkdown/src/components/Editor.vue
@@ -3,20 +3,28 @@
     <h1> エディター画面 </h1>
     <span>{{ user.displayName }}</span>
     <button @click="logout"> ログアウト </button>
+    ​<textarea​class="markdown" v-model="markdown"></textarea>
+    ​  ​​​​<div​class="preview"​v-html="preview()"></div>​
+    ​​</div>
   </div>
 </template>
 <script>  
+import marked from 'marked';
 export default {
   name: 'editor',
   props: ['user'],
   data () {
       return {
+        markdown: "",
       }
   },
   methods: {
     logout: function() {
       firebase.auth().signOut();
     }
+  },
+  preview: function(){
+    return marked(this.markdown);
   }
 }
 </script>
